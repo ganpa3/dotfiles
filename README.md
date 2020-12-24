2d05ead0abbe941f4c2a3311093d2796bcc07332
ganpa <pawarg256@gmail.com>
2020-12-24 14:52:36 +0530

    Changed

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ To use, run the following command:
 ```
 sh -c "$(curl -fsS https://raw.githubusercontent.com/ganpa3/dotfiles/main/install.sh)"
 ```
+This will replace your current configs.
--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-git clone --separate-git-dir=$HOME/.dotfiles https://github.com/ganpa3/dotfiles.git $HOME/tmp
-rsync --recursive --verbose --exclude '.git' ~/tmp/ $HOME/
-rm --recursive --force ~/tmp
+git clone --separate-git-dir=$HOME/.dotfiles https://github.com/ganpa3/dotfiles.git $HOME/tmp-dotfiles
+rsync --recursive --verbose --exclude '.git' $HOME/tmp-dotfiles/ $HOME/
+rm --recursive --force $HOME/tmp-dotfiles
