7579af36394c9cb4269baac5f06cc629ac393a59
Ganesh Pawar <58626718+ganpa3@users.noreply.github.com>
2020-12-28 21:54:42 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@
 
 INSTALL_DIR=$HOME/tmp-dotfiles
 
-git clone --separate-git-dir=$HOME/.dotfiles https://github.com/ganpa3/dotfiles.git $INSTALL_DIR
-rsync --recursive --verbose --exclude '.git' $INSTALL_DIR/ $HOME/
-rm --recursive --force $INSTALL_DIR
+if [ "$1" == "ssh" ]; then
+    echo "YES"
+  #  git clone --separate-git-dir=$HOME/.dotfiles git@github.com:ganpa3/dotfiles.git $INSTALL_DIR
+else
+    echo "NO"
+#    git clone --separate-git-dir=$HOME/.dotfiles https://github.com/ganpa3/dotfiles.git $INSTALL_DIR
+fi
+
+#rsync --recursive --verbose --exclude '.git' $INSTALL_DIR/ $HOME/
+#rm --recursive --force $INSTALL_DIR
