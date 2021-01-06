4cf3453c045d5229968ff351c695752e50795eb1
Ganesh Pawar <pawarg256@gmail.com>
2021-01-06 14:33:14 +0530

    Changed

--- install.sh
+++ install.sh
@@ -7,10 +7,18 @@ if [ -d "$HOME/.dotfiles" ]; then
     rm --recursive --force "$HOME/.dotfiles"
 fi
 
-if [ "$1" == "ssh" ]; then
-    git clone --separate-git-dir=$HOME/.dotfiles git@github.com:ganpa3/dotfiles.git $INSTALL_DIR
+if [[ "$1" == "minimal" ]]; then
+	if [ "$2" == "ssh" ]; then
+	    git clone -b minimal --single-branch --separate-git-dir=$HOME/.dotfiles git@github.com:ganpa3/dotfiles.git $INSTALL_DIR
+	else
+	    git clone -b minimal --single-branch --separate-git-dir=$HOME/.dotfiles https://github.com/ganpa3/dotfiles.git $INSTALL_DIR
+	fi
 else
-    git clone --separate-git-dir=$HOME/.dotfiles https://github.com/ganpa3/dotfiles.git $INSTALL_DIR
+	if [ "$1" == "ssh" ]; then
+	    git clone --separate-git-dir=$HOME/.dotfiles git@github.com:ganpa3/dotfiles.git $INSTALL_DIR
+	else
+	    git clone --separate-git-dir=$HOME/.dotfiles https://github.com/ganpa3/dotfiles.git $INSTALL_DIR
+	fi
 fi
 
 rsync --recursive --verbose --exclude '.git' $INSTALL_DIR/ $HOME/
