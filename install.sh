fb58c482e18815a2a157de2e7f03176faf8f67f7
Ganesh Pawar <pawarg256@gmail.com>
2021-01-09 21:42:59 +0530

    Changed

--- install.sh
+++ install.sh
@@ -2,19 +2,34 @@
 
 INSTALL_DIR=$HOME/tmp-dotfiles
 
+OPTIONS=$(getopt --options ms --longoptions minimal,ssh --name "$0" -- "$@")
+eval set -- "$OPTIONS"
+
+MINMAL=false
+SSH=false
+
+while true; do
+    case "$1" in
+        -m | --minimal ) MINIMAL=true; shift ;;
+        -s | --ssh ) SSH=true; shift ;;
+        -- ) shift; break ;;
+        * ) echo "DOBA! Sahi arguments de."; break ;;
+    esac
+done
+
 ## Remove existing .dotfiles repo.
 if [[ -d "$HOME/.dotfiles" ]]; then
     rm --recursive --force "$HOME/.dotfiles"
 fi
 
-if [[ "$1" == "minimal" ]]; then
-	if [[ "$2" == "ssh" ]]; then
+if [[ "$MINIMAL" == "true" ]]; then
+	if [[ "$SSH" == "true" ]]; then
 	    git clone -b minimal --single-branch --separate-git-dir=$HOME/.dotfiles git@github.com:ganpa3/dotfiles.git $INSTALL_DIR
 	else
 	    git clone -b minimal --single-branch --separate-git-dir=$HOME/.dotfiles https://github.com/ganpa3/dotfiles.git $INSTALL_DIR
 	fi
 else
-	if [[ "$1" == "ssh" ]]; then
+	if [[ "$SSH" == "true" ]]; then
 	    git clone --separate-git-dir=$HOME/.dotfiles git@github.com:ganpa3/dotfiles.git $INSTALL_DIR
 	else
 	    git clone --separate-git-dir=$HOME/.dotfiles https://github.com/ganpa3/dotfiles.git $INSTALL_DIR
