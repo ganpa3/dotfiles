ee2e4e9c959d9d60f0beb27dae5857605dd3493f
Ganesh Pawar <pawarg256@gmail.com>
2021-05-27 17:32:24 +0530

    Changed

---
+++ C++_Programs/bits/stdc++.h
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <bitset>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <iomanip>
